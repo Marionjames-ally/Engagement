15be8565c4a08f514a2366d064641588d7dff5e7
faith gakori <faithgakori506@gmail.com>
2019-12-04 17:51:17 +0300

    add auth routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,15 @@
 from flask import render_template, flash,request,url_for,redirect
 from flask_login import login_user,logout_user,login_required
 from . import auth
+from .forms import LoginForm,RegistrationForm
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
+        # db.session.add(user)
+        # db.session.commit()
     
         return redirect(url_for('auth.login'))
         title = "New Account"
@@ -20,8 +20,7 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email= login_form.email.data).first()
-        
-
+    
         login_user(user,login_form.remember.data)
         return redirect(request.args.get('next') or url_for('main.index'))
     
