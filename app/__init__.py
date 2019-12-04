3b808a78ca9fc6f5f87afeebfe930ed954be83e9
faith gakori <faithgakori506@gmail.com>
2019-12-04 17:48:30 +0300

    add csrf

--- app/__init__.py
+++ app/__init__.py
@@ -3,25 +3,29 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.loginview = 'auth.login'
 
 bootstrap = Bootstrap()
+csrf=CSRFProtect()
 
 def create_app(config_name):
     
+    app= Flask(__name__)
+
     #create app configs
-    
     app.config.from_object(Config)
     app.config.from_object(config_options[config_name])
+    app.config['SECRET_KEY']='d686414d5eeb7d38df7e8c385b2c2c47'
     
     #initializing
     bootstrap.init_app(app)
+    csrf.init_app(app)
     
     #registering
-    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
