073e40ada02d9e81ef0ce55aa45510ec7d3ebcd0
faith gakori <faithgakori506@gmail.com>
2019-12-05 08:18:14 +0300

    import migrations and initialize

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 from app import create_app,db
 from flask_script import Manager,Server,Shell
 from app.models import User
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
 
+migrate = Migrate(app,db)
 manager = Manager(app)
 
 manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
