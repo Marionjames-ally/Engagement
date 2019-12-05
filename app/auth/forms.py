135ef88af02472f8c582224b0a60fc1b38301dae
Marionjames-ally <marionjame2018@gmail.com>
2019-12-05 08:35:29 +0300

    resolve conflicts

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import (BooleanField, PasswordField, StringField, SubmitField,ValidationError)
-from wtforms.validators import Email, EqualTo, Required
+from wtforms import StringField,PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired,Email,Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
+    
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    password = PasswordField('Password',validators=[DataRequired(),EqualTo('confirm_password')])
+    
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()]) 
+    
+    submit = SubmitField('sign up')
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    
+    submit = SubmitField('Login')
 
