9ad23c1952b1522d08a41cc872432d1f2352016d
faith gakori <faithgakori506@gmail.com>
2019-12-05 13:05:59 +0300

    create comments routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
-from flask import render_template 
+from flask import render_template,redirect,url_for,request,abort
 from . import main
 from app import db 
 from .. import db
-from ..models import User
+from ..models import User,Admin,Parent,Comment
+from .forms import commentForm
+import datetime
+from flask_login import login_required,current_user
 
 @main.route('/')
 def index():
@@ -18,7 +21,27 @@ def admin():
     '''
     view root page function that returns thee index and its data
     '''
-    # title = admins portal   
+    title = 'admins portal'  
     
-    return render_template('admin.html')
+    return render_template('admin.html', title=title)
+
+@main.route('/parent/<int:id>', methods = ['GET','POST'])
+def parent():
+    '''
+    view root page function that returns the index and its data
+    ''' 
     
+    form = commentForm()
+    if form.validate_on_submit():
+        body=form.body.data
+        
+        new_comment=Comment(body=body,posted_by=current_user.username,parent_id=id)
+        
+        new_comment.save_comment()
+        
+        return redirect(url_for('.comments', id=id))
+    
+    title = 'parent portal' 
+    comments=Comment.get_comments(id)   
+    
+    return render_template('parent.html', title=title,comments=comments,form=form)
