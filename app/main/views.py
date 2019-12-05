e93c451a55d267a47d9ccf02c5424947a5450596
faith gakori <faithgakori506@gmail.com>
2019-12-05 10:16:50 +0300

    ..

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,8 @@
 from flask import render_template 
 from . import main
-# from app import db 
-# from .. import db
-# from ..models import User
-
+from app import db 
+from .. import db
+from ..models import User
 
 @main.route('/')
 def index():
@@ -13,4 +12,13 @@ def index():
     
     title = 'school'
     return render_template('index.html')
+
+@main.route('/admin')
+def admin():
+    '''
+    view root page function that returns thee index and its data
+    '''
+    # title = admins portal   
+    
+    return render_template('admin.html')
     
