1c4f9d98261206457dc29be441f6dc0e7d628474
faith gakori <faithgakori506@gmail.com>
2019-12-05 08:02:48 +0300

    include db base add, commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,8 +9,8 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        # db.session.add(user)
-        # db.session.commit()
+        db.session.add(user)
+        db.session.commit()
     
         return redirect(url_for('auth.login'))
         title = "New Account"
