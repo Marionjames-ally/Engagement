27bf2f7e80fff26348a1fde92265af52fb025729
faith gakori <faithgakori506@gmail.com>
2019-12-05 08:01:58 +0300

    create class user

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
-# from . import db
+from . import db
+from datetime import datetime
 
-# class User(db.Model):
+class User(db.Model):
+    __tablename__ = 'users'
     
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer,primary_key = True)
-#     username = db.Column(db.String(255))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    date_joined = db.Column(db.DateTime,default=datetime.utcnow)
 
-#     def __repr__(self):
-#         return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
