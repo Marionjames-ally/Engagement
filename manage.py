b8b5d916071a49fd2cf34826c30019938168e786
faith gakori <faithgakori506@gmail.com>
2019-12-05 13:05:31 +0300

    add classes

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server,Shell
-from app.models import User,Admin
+from app.models import User,Admin,Parent,Comment
 from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
@@ -13,7 +13,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,Parent=Parent,Admin=Admin )
 
 if __name__ == '__main__':
     manager.run()
