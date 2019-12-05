cee74c1da053555f44d096521cea070e5070a1f5
faith gakori <faithgakori506@gmail.com>
2019-12-05 13:07:14 +0300

    create relationship between comments and parent

--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
 from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    function that queries the database to check if user exists
+    '''
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer,primary_key = True)
@@ -10,6 +19,7 @@ class User(db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     date_joined = db.Column(db.DateTime,default=datetime.utcnow)
     pass_secure = db.Column(db.String(255))
+    parent=db.relationship('Parent', backref='Parent', lazy='dynamic')
     
     @property
     def password(self):
@@ -25,7 +35,7 @@ class User(db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-class Admin():
+class Admin(db.Model):
     __tablename__= 'admin'
     
     id = db.Column(db.Integer,primary_key = True)
@@ -38,12 +48,67 @@ class Admin():
         db.session.add(self)
         db.session.commit()
         
-class Parent():
+class Parent(db.Model):
     __tablename__='parent'
     
     id = db.Column(db.Integer,primary_key = True)
-    parent_title = db.Column(db.String(255))
-    parent_content = db.Column(db.String(5000))
+    title = db.Column(db.String(255))
+    body = db.Column(db.String(5000))
+    posted_on=db.Column(db.DateTime,default=datetime.utcnow)
+    posted_by=db.Column(db.String(255))
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+    comment=db.relationship('Comment',backref='comment',lazy='dynamic')
     
+    def save_parent(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    
+class Comment(db.Model):
+    __tablename__='comments'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    body = db.Column(db.String(255))
+    posted_by=db.Column(db.String(255))
+    posted_on=db.Column(db.DateTime,default=datetime.utcnow)
+    
+    def save_comment(self):
+        '''
+        function that saves a new comment to the database
+        '''
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_comments(cls,id):
+        '''
+        function that get comments for a parent 
+        '''
+        comments =  Comment.query.filter_by(parent_id=id).all()
+        return comments
+    
+    def delete_comment(self):
+        '''
+        function that deletes a comment from database 
+        '''
+        db.session.delete(self)
+        db.session.commit()
+        
+        def __repr__(self):
+            '''
+            function that helps in dec=bugging
+            '''
+            return f'Comment {self.body}'
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+        
     
     
