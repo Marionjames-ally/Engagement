1bba5c1747e133b66dcff82e460b3bd8a0048150
Marionjames-ally <marionjames2018@gmail.com>
2019-12-06 09:29:20 +0300

    fix a bug

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,8 +1,4 @@
 from flask import Blueprint
 auth = Blueprint('auth',__name__)
 
-<<<<<<< HEAD
 from . import views
-=======
-from . import views,forms
->>>>>>> 4fb246d9db5c093755bbcd20e4c85fe5496b7416
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import FlaskForm
-<<<<<<< HEAD
 from wtforms import StringField,PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired,Email,Length, EqualTo
 
@@ -21,21 +20,3 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     
     submit = SubmitField('Login')
-=======
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
-from wtforms.validators import Required,Email,EqualTo
-from ..models import User
-
-
-
-
-
-
-
-
-class LoginForm(FlaskForm):
-  email = StringField("Your Email Address",validators=[Required(),Email()])
-    password = PasswordField("Password",validators=[Required()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField("Sign In")
->>>>>>> 4fb246d9db5c093755bbcd20e4c85fe5496b7416
